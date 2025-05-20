bab11b3f2caadcd7294e6a1af1d3b0ff4bc0f0f3
Dierdier13 <didier.piroille@gmail.com>
2025-05-20 12:15:41 +0200

    refonte color

--- public/assets/js/map4.js
+++ public/assets/js/map4.js
@@ -5,6 +5,13 @@ const longitude = parseFloat(mapElement.getAttribute("data-lon"));
 const treeName = mapElement.getAttribute("data-name");
 const sector = mapElement.getAttribute("data-sector");
 
+const customIcon = L.icon({
+    iconUrl: '/assets/images/arbre.png',
+    iconSize: [38, 48],
+    iconAnchor: [22, 47],
+    popupAnchor: [-3, -76]
+  });
+
 const map = L.map('map-4', {
     zoomControl: false,
     dragging: true,
@@ -17,5 +24,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-const marker = L.marker([latitude, longitude]).addTo(map);
+const marker = L.marker([latitude, longitude], { icon: customIcon }).addTo(map);
 marker.bindPopup(`<strong>${treeName}</strong><br>Secteur : ${sector}`).openPopup();
